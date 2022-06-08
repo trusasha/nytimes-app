bbcfc544815bfb943688cce001f8cad8ce7a548c
trusasha <atr@nkhstudio.ru>
2022-06-08 18:23:53 +0400

    ActionModal: added animations

--- src/components/ActionModal/index.js
+++ src/components/ActionModal/index.js
@@ -1,7 +1,31 @@
-import React, { useRef, useEffect, useCallback } from 'react';
+import React, { useRef, useEffect, useCallback, useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import useStyles from './useStyles';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import { useSpecialStyleProps } from 'hooks/newUseStyles';
+
+const minimalVerticalOffset = 8;
+const minimalHorizontalOffset = 12;
+
+/**
+ * @typedef {{
+ * xLeft: number
+ * xRight: number
+ * yTop: number
+ * yBottom: number
+ * horizontalOffsets: number
+ * }} Measures
+ */
+
+/** @type {Measures} */
+const initialMeasures = {
+  xLeft: 0,
+  xRight: 0,
+  yTop: 0,
+  yBottom: 0,
+  horizontalOffsets: 0,
+};
 
 /**
  * @typedef {{
@@ -17,49 +41,109 @@ import useStyles from './useStyles';
  * @param {ActionModalProps} props
  */
 const ActionModal = ({ visible, setVisible, children, offset, modalComponent }) => {
+  const S = useStyles();
+  const { w, h } = useSpecialStyleProps();
+
+  const [measures, setMeasures] = useState(initialMeasures);
+
   /** @type {React.MutableRefObject<import('react-native').LayoutRectangle>} */
   const childrenLayout = useRef(null);
   /** @type {React.MutableRefObject<import('react-native').LayoutRectangle>} */
   const modalLayout = useRef(null);
-
-  const rightPosition =
-    ((modalLayout?.current?.width || 0) - (childrenLayout?.current?.width || 0)) / -2;
-
-  const S = useStyles({ childrenLayout: childrenLayout?.current, offset });
+  /** @type {React.MutableRefObject<Measures>} */
+  const childrenLayoutMeasures = useRef(null);
 
   /** @type {React.MutableRefObject<View>} */
   const childrenRef = useRef(null);
 
-  /** @type {React.MutableRefObject<View>} */
+  /** @type {React.MutableRefObject<Animated.View>} */
   const modalRef = useRef(null);
+
+  const modalTopOffset = (childrenLayout?.current?.height || 0) + (offset || minimalVerticalOffset);
+  const modalSideOffset =
+    measures.xRight < minimalHorizontalOffset
+      ? -measures.horizontalOffsets
+      : measures.xLeft < minimalHorizontalOffset
+      ? measures.horizontalOffsets
+      : 0;
+
+  useEffect(() => {
+    if (
+      visible &&
+      modalLayout.current &&
+      childrenLayout.current &&
+      childrenLayoutMeasures.current
+    ) {
+      const horizontalOffsets = (modalLayout.current.width - childrenLayout.current.width) / 2;
+
+      setMeasures({
+        xLeft:
+          childrenLayoutMeasures.current.xLeft - childrenLayout.current.width - horizontalOffsets,
+        xRight: childrenLayoutMeasures.current.xRight - horizontalOffsets,
+        yTop: 0,
+        yBottom: 0,
+        horizontalOffsets: horizontalOffsets,
+      });
+    }
+  }, [visible]);
+
+  const progress = useSharedValue(0);
+
+  const reanimatedStyle = useAnimatedStyle(
+    () => ({
+      opacity: progress.value,
+      transform: [
+        { scale: progress.value },
+        { translateY: progress.value * modalTopOffset },
+        { translateX: progress.value * modalSideOffset },
+      ],
+    }),
+    [modalTopOffset, modalSideOffset],
+  );
+
+  const modalStyles = [S.container, reanimatedStyle];
+  const pointerEvents = visible ? 'auto' : 'none';
 
   /** @type {(event: import('react-native').LayoutChangeEvent) => void} */
   const onChildrenLayout = useCallback(({ nativeEvent }) => {
     childrenLayout.current = nativeEvent.layout;
-    console.log('LOG CHILDREN: ', nativeEvent);
   }, []);
 
   /** @type {(event: import('react-native').LayoutChangeEvent) => void} */
-  const onModalLayout = useCallback(({ nativeEvent }) => {
-    modalLayout.current = nativeEvent.layout;
-    console.log('LOG MODAL: ', nativeEvent);
-  }, []);
+  const onModalLayout = useCallback(
+    ({ nativeEvent, target }) => {
+      modalLayout.current = nativeEvent.layout;
+      // @ts-ignore
+      target.measure((x, y, width, height, pageX, pageY) => {
+        childrenLayoutMeasures.current = {
+          xLeft: Math.round(x + pageX),
+          xRight: w - Math.round(x + pageX),
+          yTop: Math.round(y + pageY),
+          yBottom: Math.round(y + pageY) - h,
+          horizontalOffsets: 0,
+        };
+      });
+    },
+    [h, w],
+  );
 
   const onClose = useCallback(() => setVisible(false), [setVisible]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    progress.value = withTiming(visible ? 1 : 0, { duration: 150 });
+  }, [progress, visible]);
 
   return (
     <View ref={childrenRef} onLayout={onChildrenLayout}>
       {children}
-      {visible && (
-        <>
-          <View style={S.container} ref={modalRef} onLayout={onModalLayout}>
-            {modalComponent}
-          </View>
-          <TouchableOpacity style={S.backdrop} onPress={onClose} />
-        </>
-      )}
+      <Animated.View
+        style={modalStyles}
+        ref={modalRef}
+        pointerEvents={pointerEvents}
+        onLayout={onModalLayout}>
+        {modalComponent}
+      </Animated.View>
+      {visible && <TouchableOpacity style={S.backdrop} onPress={onClose} />}
     </View>
   );
 };
--- src/components/ActionModal/useStyles.js
+++ src/components/ActionModal/useStyles.js
@@ -1,28 +1,23 @@
 import newUseStyles from 'hooks/newUseStyles';
 
-const useStyles = newUseStyles(
-  (
-    { p, w, h, l, n, i },
-    /** @type {{childrenLayout: import('react-native').LayoutRectangle, offset: number}} */
-    { childrenLayout, offset },
-  ) => {
-    const containerTop = (childrenLayout?.height || 0) + (offset || 8);
-
-    return {
-      container: {
-        position: 'absolute',
-        top: containerTop,
-        alignSelf: 'center',
-        zIndex: 1,
-      },
-      backdrop: {
-        position: 'absolute',
-        left: -w,
-        width: w * 2,
-        height: h,
-      },
-    };
-  },
-);
+const useStyles = newUseStyles(({ p, w, h, l, n, i }) => {
+  return {
+    container: {
+      position: 'absolute',
+      top: 0,
+      alignSelf: 'center',
+      zIndex: 1,
+      shadowRadius: 8,
+      shadowOpacity: 0.2,
+      shadowColor: 'black',
+    },
+    backdrop: {
+      position: 'absolute',
+      left: -w,
+      width: w * 2,
+      height: h,
+    },
+  };
+});
 
 export default useStyles;
