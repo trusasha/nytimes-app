8424cd0af6b97a8c34c53ce6c0c0fc2a3cc72aa0
trusasha <atr@nkhstudio.ru>
2022-06-12 16:08:03 +0400

    HomeStoriesItem: added header & preview modal

--- src/screens/Home/HomeStoriesItem/index.js
+++ src/screens/Home/HomeStoriesItem/index.js
@@ -6,11 +6,11 @@ import { configStore } from 'stores';
 import dayjs from 'dayjs';
 import AnimatedModal from 'components/AnimatedModal';
 import HomeStoryModal from '../HomeStoryModal';
+import HomeStoryPreviewModal from '../HomeStoryPreviewModal';
 
 /**
  * @typedef {{
  *  item: Story
- *  index: number
  *  offset: number
  * }} HomeStoriesItemProps
  */
@@ -18,12 +18,13 @@ import HomeStoryModal from '../HomeStoryModal';
 /**
  * @param {HomeStoriesItemProps} props
  */
-const HomeStoriesItem = ({ item, index, offset }) => {
+const HomeStoriesItem = ({ item, offset }) => {
   const { title, byline, created_date, multimedia } = item;
   const T = configStore.localization;
   const S = useStyles();
 
   const [storyVisible, setStoryVisible] = useState(false);
+  const [previewVisible, setPreviewVisible] = useState(false);
 
   /** @type {React.MutableRefObject<TouchableOpacity>} */
   const ref = useRef(null);
@@ -31,32 +32,54 @@ const HomeStoriesItem = ({ item, index, offset }) => {
   const image = { uri: multimedia?.[0]?.url };
   const timeFrom = `${T.published}: ${dayjs(created_date).fromNow()}`;
 
-  /** @type {(item: Story, ref: React.MutableRefObject<TouchableOpacity>) => void} */
-  const onOpenModal = useCallback((item, ref) => {
-    setStoryVisible(true);
-  }, []);
+  const onModalOpen = useCallback(() => setStoryVisible(true), []);
+  const onModalClose = useCallback(() => setStoryVisible(false), []);
 
-  const onPress = useCallback(() => onOpenModal(item, ref), [item, onOpenModal]);
+  const onPreviewOpen = useCallback(() => setPreviewVisible(true), []);
+  const onPreviewClose = useCallback(() => setPreviewVisible(false), []);
 
   const modal = useMemo(() => <HomeStoryModal item={item} />, [item]);
+  const modalHeader = useMemo(
+    () => (
+      <View style={S.header}>
+        <TouchableOpacity style={S.backButton} onPress={onModalClose} />
+        <Text style={S.headerTitle} children={title} numberOfLines={1} />
+      </View>
+    ),
+    [S.backButton, S.header, S.headerTitle, onModalClose, title],
+  );
+
+  const preview = useMemo(() => <HomeStoryPreviewModal item={item} />, [item]);
 
   const offsets = { top: -offset };
 
   return (
     <View style={S.container}>
       <AnimatedModal
-        visible={storyVisible}
-        setVisible={setStoryVisible}
-        modal={modal}
+        visible={previewVisible}
+        setVisible={setPreviewVisible}
+        modal={preview}
         offsets={offsets}>
-        <TouchableOpacity ref={ref} style={S.itemContainer} onPress={onPress}>
-          <Image style={S.image} source={image} />
-          {/* <View style={S.content}>
-            <Text style={S.title} children={title} numberOfLines={2} />
-            <Text style={S.byline} children={byline} numberOfLines={2} />
-            <Text style={S.published} children={timeFrom} numberOfLines={2} />
-          </View> */}
-        </TouchableOpacity>
+        <AnimatedModal
+          visible={storyVisible}
+          setVisible={setStoryVisible}
+          modal={modal}
+          header={modalHeader}
+          offsets={offsets}>
+          <TouchableOpacity
+            ref={ref}
+            style={S.itemContainer}
+            onPress={onModalOpen}
+            onLongPress={onPreviewOpen}
+            delayLongPress={300}>
+            <Image style={S.image} source={image} />
+            {/* <View style={S.content}>
+              <Text style={S.title} children={title} numberOfLines={2} />
+              <Text style={S.byline} children={byline} numberOfLines={2} />
+              <Text style={S.published} children={timeFrom} numberOfLines={2} />
+            </View> */}
+          </TouchableOpacity>
+        </AnimatedModal>
       </AnimatedModal>
     </View>
   );
--- src/screens/Home/HomeStoriesItem/useStyles.js
+++ src/screens/Home/HomeStoriesItem/useStyles.js
@@ -45,6 +45,27 @@ const useStyles = newUseStyles(({ p, w, h, l, n, i }) => {
     published: {
       ...getTextStyles(12, '400', 18, p.strokeSecondary),
     },
+    header: {
+      padding: 12,
+      height: 60,
+      borderBottomWidth: 1,
+      borderBottomColor: p.border,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    backButton: {
+      width: 40,
+      height: 40,
+      backgroundColor: p.border,
+      position: 'absolute',
+      left: 12,
+      zIndex: 10,
+    },
+    headerTitle: {
+      paddingHorizontal: 48,
+      ...getTextStyles(14, '700', 22, p.stroke),
+    },
   };
 });
 
